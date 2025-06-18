9f434f27b47d08c3311ad70e66822c1000b5c9cc
loki0b <contato@henriquegarcialeite.com>
2025-06-18 17:27:59 -0300

    update scripts

--- script0.sh
+++ script0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pacman -Sy archlinux-keyring --noconfirm
-pacstrap -K /mnt base linux linux-firmware iptables-nft intel-ucode vim man-db man-pages texinfo
+pacstrap -K /mnt base linux linux-firmware nftables iptables-nft intel-ucode vim man-db man-pages texinfo
 genfstab -U /mnt >> /mnt/etc/fstab
 cp script1.sh /mnt
 arch-chroot /mnt
