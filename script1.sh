b172de6e1d127d3432b64153b8744268bf53fa33
loki0b <social@henriquegarcialeite.com>
2025-07-09 20:41:44 -0300

    update

--- script1.sh
+++ script1.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# TODO:
+# Always check before run a command
+# activate wireless-regdb
+#pacman -S dosfstools btrfs-progs e2fsprogs fschk helper to mkinitcpio
+
 ln -sf /usr/share/zoneinfo/America/Sao_paulo /etc/localtime
 hwclock --systohc
 
+# choose keymap and lang
 sed -i "s/#en_US.UTF-8/en_US.UTF-8/" /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 >> /etc/locale.conf
@@ -13,18 +19,17 @@ read HOSTNAME
 echo "$HOSTNAME" >> /etc/hostname
 sed -i "/# See hosts(5) for details./a 127.0.0.1\tlocaldomain\n::1\t\tlocaldomain\n127.0.1.1\t$HOSTNAME.localdomain $HOSTNAME" /etc/hosts
 
-#pacman -S dosfstools btrfs-progs e2fsprogs fschk helper to mkinitcpio
 # reflector
-#pacman -Rnsdd iptables --noconfirm
 #config iwd/main.conf
 pacman -S iwd ntp --needed --noconfirm
 
+# improve
 echo -e '[Match]\nName=*\n[Network]\nDHCP=yes' > /etc/systemd/network/99-default.network
 
 systemctl enable iwd
 systemctl enable ntpd
 systemctl enable systemd-networkd
-systemctt enable systemd-resolved
+systemctl enable systemd-resolved
 ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
 # Root password - adding users/passwords
